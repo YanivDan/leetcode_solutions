d368219355d24fcccedf1d7d61f0a01c49d817cd
Yaniv <yanivdezan@gmail.com>
2022-03-10 21:47:15 +0200

    first try of the solution, commnted the arrays of the first try

--- RotateArray/RotateArray.py
+++ RotateArray/RotateArray.py
@@ -1,21 +1,29 @@
-
+# First arrays to tey the solution
 arr1 = [1,2,3,4,5,6,7]
 arr2 = [-1,-100,3,99]
 
 def rotate(nums, k):
+    nums_length = len(nums)
+
     # doesnt matter what is k, the array will not change
-    if len(nums) == 1:
+    if nums_length == 1:
         return 
     # k is the the same size of the array, the array will stay the same
-    if len(nums) == k:
+    if nums_length == k:
         return
     
     ind = 0
-    runner = "_"
     while ind < k:
-        
-
-
+        # last number in the array
+        last_number = nums[nums_length - 1]
+        # index to run over the array
+        ind2 = nums_length
+        while ind2 > 0:
+            nums[ind2 - 1] = nums[ind2 - 2]
+            # decrese ind2 by 1
+            ind2 -= 1
+        # replace the first place with the number of the last place
+        nums[0] = last_number
         # increse i
         ind += 1
 
